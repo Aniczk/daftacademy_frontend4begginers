3602adc64eedcc9ca3d2c1f5cf799c91f81543f4
Aniczk <32680745+Aniczk@users.noreply.github.com>
2019-12-15 09:54:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
+// #######################################
+//  NAWIGACJA
+// #######################################
+
 // Hamburger menu
 
-const hamburger = document.querySelector('.page-navigation__hamburger-menu');
-const nav = document.querySelector('.page-navigation__items');
+// Pokazywanie/ukrywanie navigacji oraz zmiana hamburgera w X
+const menuButton = document.querySelector('.hamburger');
 
-hamburger.addEventListener('click', () =>{
-  nav.classList.toggle('page-navigation__items');
+menuButton.addEventListener('click', (e) => {
+    const pageNavigation = document.querySelector('.page-navigation');
+    pageNavigation.classList.toggle('page-navigation--visible');
+    menuButton.classList.toggle('change');
 });
 
-// "Have fun!" po kliknieciu w przycisk "Buy a ticket"
 
-const button = document.querySelector('.button');
+// ######################################
+// SEKCJA "CONCERTS"
+// ######################################
 
-button.addEventListener('click', () => {
-  button.classList.remove('button');
-})
+// Po kliknieciu w przycisk "Buy a ticket", zamiast przycisku pokazuje sie tekst "Have fun!"
+
+const concerts = document.querySelector('#concerts');
+
+concerts.addEventListener('click', (e) => {
+    if (e.target.classList.contains('button--buy-ticket')) {
+        const buyTicketButton = e.target;
+        const haveFunText = document.createElement('p');
+        haveFunText.textContent = 'Have fun!';
+
+        const buttonContainer = buyTicketButton.parentElement;
+        buttonContainer.insertBefore(haveFunText, buyTicketButton);
+        buyTicketButton.remove();
+        // OR
+        // buyTicketButton.replaceWith(haveFunText);
+    }
+});
+
+
+// ######################################
+// SEKCJA "CONTACT"
+// ######################################
+
+// Funkcja która na evencie "submit" formularza kontaktowego wyloguje wpisane wartości do konsoli.
+
+const contactForm = document.querySelector('.contact__form');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObject = Object.fromEntries(formData);
+    console.log(formObject);
+});
